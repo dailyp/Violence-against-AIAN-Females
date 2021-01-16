cefaba7e9db2373f283727fe10dfded35842b8ad
dailyp <patti.111119@gmail.com>
2021-01-16 11:25:16 -0600

    app working for death types and metro types

--- server.R
+++ server.R
@@ -1,16 +1,15 @@
 
-
 server <- function(input, output, session) {
     output$us_death2 <- renderPlot({
         us_death2 %>%
-          filter(Cause_death == input$death) %>%
-          filter(Metro_type == input$metro) %>%
+            filter(Cause_death == input$death) %>%
+            filter(Metro_type == input$metro) %>%
             # filter(hispanic == input$hispanic)
-        ggplot(aes(x = Race, y = Crude_rate)) +
+            ggplot(aes(x = Race, y = Crude_rate)) +
             geom_col()
     })
     
-    # output$us_death2 <- renderPlot({
+    #output$death_trends <- renderPlot({
     #   us_death2 %>%
     #     filter(Cause_death == input$death) %>%
     #     #filter(Metro_type == input$metro) %>%
