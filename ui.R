2222e705637245ab76762b26533fa147fa0bffa9
dailyp <patti.111119@gmail.com>
2021-01-16 11:24:03 -0600

    app working for deaths type and metro type

--- ui.R
+++ ui.R
@@ -3,40 +3,67 @@ ui <- navbarPage(title = "MMIWG Crisis",
                  
                  tabPanel("Violent Deaths",
                           sidebarLayout(
-                              sidebarPanel(
-                                  # Input: type of death  ----
-                                  radioButtons("death",
-                                               "Type of Violent Death:",
-                                               c("Homicide" = "Homicide",
-                                                 "Suicide" = "Suicide",
-                                                 "Both" = "All Violent Deaths")),
-                                  
-                                  hr(),
-                                  #Input: Metro or NonMetro
-                                  radioButtons("metro", "Metro or Non-metro:",
-                                               c("Metro" = "Metro",
-                                                 "Non Metro" = "Non Metro",
-                                                 "All Metros" = "All Metros")),
-                                  
-                                  #hr(),
-                                  #Input: Hispanic combined and separated 
-                                  #radioButtons("hispanic", "Show Hispanic vs Non Hispanic:",
-                                               #c("Hispanic" = "hispanic",
-                                                 #"Not Hispanic" = "nothispanic")),
-                                  
-                              ),
+                            sidebarPanel(
+                              # Input: type of death  ----
+                              radioButtons("death",
+                                           "Type of Violent Death:",
+                                           c("Homicide" = "Homicide",
+                                             "Suicide" = "Suicide",
+                                             "Both" = "All Violent Deaths")),
                               
-                              # Main panel for displaying Death outputs ----
-                              mainPanel(
-                                  plotOutput("us_death2", height = 200) 
-                              )
+                              hr(),
+                              #Input: Metro or NonMetro
+                              radioButtons("metro", "Metro or Non-metro:",
+                                           c("Metro" = "Metro",
+                                             "Non Metro" = "Non Metro",
+                                             "All Metros" = "All Metros")),
+                              
+                            ),
+                            
+                            # Main panel for displaying Death outputs ----
+                            mainPanel(
+                              plotOutput("us_death2", height = 200) 
+                            )
                           )
                  ),
                  
                  tabPanel("Missing", plotOutput("missing")),
-                 tabPanel("Map", plotOutput("map")),
-                 tabPanel("Citations", verbatimTextOutput("citations"))
-)
+                 
+                 tabPanel("Map", 
+                          sidebarLayout(
+                            sidebarPanel(
+                              # Input: U.S. map showing viol deaths & missing
+                              radioButtons("death",
+                                           "Type of Violent Death:",
+                                           c("Homicide" = "Homicide",
+                                             "Suicide" = "Suicide",
+                                             "Both" = "All Violent Deaths")),
+                              
+                              hr(),
+                              #Input: Metro or NonMetro
+                              radioButtons("metro", "Metro or Non-metro:",
+                                           c("Metro" = "Metro",
+                                             "Non Metro" = "Non Metro",
+                                             "All Metros" = "All Metros")),
+                              
+                              #hr(),
+                              #Input: Hispanic combined and separated 
+                              #radioButtons("hispanic", "Show Hispanic vs Non Hispanic:",
+                              #c("Hispanic" = "hispanic",
+                              #"Not Hispanic" = "nothispanic")),
+                              
+                            ),
+                            
+                            # Main panel for displaying Death outputs ----
+                            mainPanel(
+                              #plotOutput("us_death2", height = 200) 
+                            )
+                          )
+                 ))
+                 #plotOutput("map"))
+#tabPanel("Citations", verbatimTextOutput("citations"))
+
+
 
 
 
