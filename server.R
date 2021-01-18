6dc0dd3481d8d9df00409fcea07dffcf727c9620
dailyp <patti.111119@gmail.com>
2021-01-18 16:38:04 -0600

    added death trends

--- server.R
+++ server.R
@@ -9,14 +9,28 @@ server <- function(input, output, session) {
             geom_col()
     })
     
-    #output$death_trends <- renderPlot({
-    #   us_death2 %>%
-    #     filter(Cause_death == input$death) %>%
-    #     #filter(Metro_type == input$metro) %>%
-    #     # filter(hispanic == input$hispanic)
-    #     ggplot(aes(x = Race, y = Crude_rate)) +
-    #     geom_col()
-    # })
+    output$death_trend <- renderPlot({
+        us_death2 %>%
+            filter(Cause_death == input$death) %>%
+            filter(Metro_type == input$metro) %>%
+            ggplot(aes(x = Year, y = .data[[input$trend]], color = Race)) +
+            geom_line()
+    })
+    
+     
+    
+    output$maps <- renderPlot({
+        viol_death_map %>% 
+            state_choropleth(viol_death_map, 
+                             num_colors=9) +
+            scale_fill_brewer(palette="YlOrRd") +
+            labs(title = "AIAN Female Violent Deaths by State, 2001-2018",
+                 #subtitle = "",
+                 caption = "source: CDC",
+                 fill = "Events")
+    })
+    
+
 }
 
 
