c80ce88bdf04e254cbbe74f8303eae4790f049e0
dailyp <patti.111119@gmail.com>
2021-01-22 00:45:05 -0600

    map and missing tabs and color updated

--- server.R
+++ server.R
@@ -4,10 +4,11 @@ server <- function(input, output, session) {
         us_death2 %>%
             filter(Cause_death == input$death) %>%
             filter(Metro_type == input$metro) %>%
-            # filter(hispanic == input$hispanic)
             ggplot(aes(x = Race, y = .data[[input$trend]], fill = Race)) +
             geom_bar(stat = "identity")+
-            scale_fill_brewer(palette = "Dark2")
+            scale_fill_hue(l=40, c=35)
+            #scale_fill_brewer(palette = "YlOrBr")
+            #scale_fill_manual(values=wes_palette(n=3, name="GrandBudapest"))
     })
     
     output$death_trend <- renderPlot({
@@ -16,36 +17,26 @@ server <- function(input, output, session) {
             filter(Metro_type == input$metro) %>%
             ggplot(aes(x = Year, y = .data[[input$trend]], color = Race)) +
             geom_line()+
-            scale_color_brewer(palette = "Dark2")
+            scale_color_hue(l=40, c=35)
     })
     
-    output$missing <- renderPlot({
-        ncic_proport %>% 
-            ggplot(aes(x=Year, y=Percentage, fill=Race)) + 
-            geom_area(alpha=0.6, size=1)+
-            scale_fill_brewer(palette = "Dark2")
-    })
+    #output$missing <- renderPlot({
+        # ncic_proport %>% 
+        #     ggplot(aes(x=Year, y=Percentage, fill=Race)) + 
+        #     geom_area(alpha=0.6, size=1)+
+        #     scale_fill_brewer(palette = "Dark2")
+    #})
     
     output$missing_pop <- renderPlot({
-        ncic_stacked %>% 
-            ggplot(aes(x = Year, y = Count, fill = Race)) +
-            geom_area(alpha=0.6 , size=1)+
-            scale_fill_brewer(palette = "Dark2")
+        ncic_pop_proport %>% 
+            ggplot(aes(x = Year, y = .data[[input$proportion]], color = Race)) +
+            geom_line(size=1)+
+            scale_color_hue(l=40, c=35)
+            #scale_fill_brewer(palette = "Dark2")
     })
     
-    output$map <- renderPlot({
-        # df <- read.csv("https://raw.githubusercontent.com/plotly/datasets/master/2011_us_ag_exports.csv")
-        # df_dmuu <- read_csv("data/comb_df_map.csv")
-        # 
-        # df_dmuu <- df_dmuu %>% 
-        #     rename(state = "State")
-        # 
-        # df_dmuu <- df_dmuu  %>% 
-        #     inner_join(df) %>% 
-        #     select("code", "state", "value", "category")
-        # 
-        #df_dmuu$hover <- with(df_dmuu, paste(state, '<br>'))
-        df_dmuu %>%
+    output$map <- renderPlotly({
+        
             l <- list(color = toRGB("white"), width = 2)
             
             g <- list(
@@ -58,11 +49,11 @@ server <- function(input, output, session) {
             fig <- plot_geo(df_dmuu, locationmode = 'USA-states')
             fig <- fig %>% add_trace(
                 z = ~value, text = ~hover, locations = ~code,
-                color = ~value, colors = 'Purples'
+                color = ~value, colorscale = 'scl'
             )
-            fig <- fig %>% colorbar(title = "Violent Deaths, Missing")
+            fig <- fig %>% colorbar(title = "Count by State")
             fig <- fig %>% layout(
-                title = 'Violent Deaths and Missing by State<br>(Hover for breakdown)',
+                title = '<br>(Hover for breakdown)',
                 geo = g
             )
             fig
